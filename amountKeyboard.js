332f3de7267d52728f1bd0f5727e83852a9a99ec
zhaoyajie <yajie.zhao@weitaige.com>
2020-06-18 17:28:24 +0800

    组织双击放大

--- amountKeyboard.js
+++ amountKeyboard.js
@@ -178,7 +178,23 @@ AmountField.prototype.val = function(value){
     })
 }
 document.onclick=function (event) {
+    //点击他处失焦
     if(!event.target.matches("*[class|='bc-amount']")){
         amount.blur();
     }
+    //阻止双击放大
+    var lastTouchEnd = 0;
+    document.addEventListener('touchstart', function(event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        } });
+    document.addEventListener('touchend', function(event) {
+        var now = (new Date()).getTime(); if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        } lastTouchEnd = now;
+    }, false);
+// 阻止双指放大
+    document.addEventListener('gesturestart', function(event) { event.preventDefault(); });
 }
+
+
